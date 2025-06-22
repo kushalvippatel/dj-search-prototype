5f594c233df20616d870041c42e0e3e1366d6da0
unknown <kushal.v.patel@gmail.com>
2025-06-22 18:06:41 +0100

    Fix: bind Flask to 0.0.0.0 and use PORT env variable for Render

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, jsonify
 import requests
 from bs4 import BeautifulSoup
 import re
+import os
 
 app = Flask(__name__)
 
@@ -278,5 +279,6 @@ def check_track():
     track_info = get_track_info(track_url)
     return jsonify(track_info)
 
-if __name__ == '__main__':
-    app.run(debug=True) 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
